1a1a15c4819332f92e52f55547156785bf135505
Sam Taseff <sam.taseff@proton.me>
2024-09-25 22:19:42 +0100

    sync

--- Runtime/DefaultStateAttribute.cs
+++ Runtime/DefaultStateAttribute.cs
@@ -2,6 +2,6 @@ using System;
 
 namespace Software.Contraband.StateMachines
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
     public class DefaultStateAttribute : Attribute { }
 }
